b6a5864e9c5ee19d2c35b78924565dcc054fa500
Matthew Muscat <matthew@mamis.com.au>
2017-05-06 14:32:07 +1000

    remove bugsnag library and integration
    Signed-off-by: Matthew Muscat <matthew@mamis.com.au>

--- app/code/community/Shippit/Shippit/Model/Logger.php
+++ app/code/community/Shippit/Shippit/Model/Logger.php
@@ -18,15 +18,12 @@ class Shippit_Shippit_Model_Logger
 {
     protected $helper;
     protected $debugMode;
-
-    public $bugsnag = false;
     protected $metaData = array();
 
     public function __construct()
     {
         $this->helper = Mage::helper('shippit');
         $this->debugMode = $this->helper->isDebugActive();
-        $this->bugsnag = Mage::helper('shippit/bugsnag')->init();
     }
 
     public function log($errorType, $message, $level = Zend_Log::DEBUG)
@@ -34,8 +31,6 @@ class Shippit_Shippit_Model_Logger
         // if debug mode is disabled, only log when the level is above notice
         if (!$this->debugMode && $level <= Zend_Log::NOTICE
             || $this->debugMode) {
-            $this->bugsnagLog($errorType, $message, $level);
-
             Mage::log($errorType . "\n" . $message, $level, 'shippit.log');
 
             if (!empty($this->metaData)) {
@@ -46,43 +41,8 @@ class Shippit_Shippit_Model_Logger
         return $this;
     }
 
-    public function bugsnagLog($errorType, $message, $level = Zend_Log::DEBUG)
-    {
-        if (!$this->bugsnag) {
-            return $this;
-        }
-
-        $this->bugsnag->notifyError($errorType, $message, $this->metaData, $this->_getBugsnagErrorLevel($level));
-
-        return $this;
-    }
-
-    public function bugsnagException($exception)
-    {
-        if (!$this->bugsnag) {
-            return $this;
-        }
-
-        return $this->bugsnag->notifyException($exception, $this->metaData);
-    }
-
-    public function _getBugsnagErrorLevel($level)
-    {
-        if ($level <= 3) {
-            return 'error';
-        }
-        elseif ($level == 4) {
-            return 'warning';
-        }
-        else {
-            return 'info';
-        }
-    }
-
     public function logException($e, $level = Zend_Log::ERR)
     {
-        $this->bugsnagException($e);
-
         Mage::log($e->getMessage(), $level, 'shippit.log');
 
         if (!empty($this->metaData)) {
