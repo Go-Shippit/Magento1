74227bc6789ae2080c21108eda45a25add847e62
Hardik Thakkar <hardik@mamis.com.au>
2017-09-07 14:09:35 +1000

    Add new source model for allwed methods
    - Added new source model for Shippit live quote allowed methods
    
    Signed-off-by: Hardik Thakkar <hardik@mamis.com.au>

---
+++ app/code/community/Shippit/Shippit/Model/System/Config/Source/Shippit/LiveQuote/Methods.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Shippit Pty Ltd
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the terms
+ * that is available through the world-wide-web at this URL:
+ * http://www.shippit.com/terms
+ *
+ * @category   Shippit
+ * @copyright  Copyright (c) Shippit Pty Ltd (http://www.shippit.com)
+ * @author     Matthew Muscat <matthew@mamis.com.au>
+ * @license    http://www.shippit.com/terms
+ */
+
+class Shippit_Shippit_Model_System_Config_Source_Shippit_LiveQuote_Methods
+{
+    /**
+     * Returns code => code pairs of attributes for all product attributes
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $methods = $this->getMethods();
+        $methodOptions = array();
+
+        foreach ($methods as $methodValue => $methodLabel) {
+            $methodOptions[] = array(
+                'label' => $methodLabel,
+                'value' => $methodValue,
+            );
+        }
+
+        return $methodOptions;
+    }
+
+    public function getMethods()
+    {
+        $methods = array(
+            'standard' => 'Standard',
+            'express' => 'Express',
+            'priority' => 'Priority',
+        );
+
+        return $methods;
+    }
+}
