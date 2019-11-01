1ecaa0e4fdc06f03e84fc98ebbdeae112715bcd2
Hardik Thakkar <hardik@mamis.com.au>
2019-11-01 16:52:12 +1100

    register shippit shopping cart name
    - register shopping cart name if shippit api key is changed/updated and if it is a valid api key
    
    Signed-off-by: Hardik Thakkar <hardik@mamis.com.au>

--- app/code/community/Shippit/Shippit/Model/Observer/System/Config.php
+++ app/code/community/Shippit/Shippit/Model/Observer/System/Config.php
@@ -16,6 +16,8 @@
 
 class Shippit_Shippit_Model_Observer_System_Config
 {
+    const SHIPPING_CART_METHOD_NAME = 'Magento 1';
+
     protected $helper;
     protected $syncShippingHelper;
     protected $api;
@@ -72,8 +74,12 @@ class Shippit_Shippit_Model_Observer_System_Config
                 );
             }
 
-            if ($apiKeyValid && $this->syncShippingHelper->isActive()) {
-                $this->registerWebhook();
+            if ($apiKeyValid) {
+                if ($this->syncShippingHelper->isActive()) {
+                    $this->registerWebhook();
+                }
+
+                $this->registerShoppingCartName();
             }
 
             // Stop Store Emulation
@@ -124,6 +130,26 @@ class Shippit_Shippit_Model_Observer_System_Config
         }
 
         return;
+    }
+
+    public function registerShoppingCartName()
+    {
+        try {
+            $requestData = new Varien_Object;
+            $requestData->setShippingCartMethodName(self::SHIPPING_CART_METHOD_NAME);
+            $merchant = $this->api->putMerchant($requestData, true);
+
+            if (property_exists($merchant, 'error')) {
+                Mage::getSingleton('adminhtml/session')->addError(
+                    $this->helper->__('The request to update the shopping cart integration name failed - please try again.')
+                );
+            }
+        }
+        catch (Exception $e) {
+            Mage::getSingleton('adminhtml/session')->addError(
+                $this->helper->__('The request to update the shopping cart integration name failed - please try again.')
+            );
+        }
     }
 
     /**
